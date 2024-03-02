2032b31a428233d2b9ddf9cdc3544cf0830b6f3b
chloeboireaudevier <cbd65@hotmail.fr>
2024-03-02 23:32:37 +0100

    Fin TP1

---
+++ src/villagegaulois/VillageSansChefException.java
@@ -0,0 +1,19 @@
+package villagegaulois;
+
+public class VillageSansChefException extends RuntimeException {
+	public VillageSansChefException() {
+		
+	}
+	
+	public VillageSansChefException(String message) {
+		super(message);
+	}
+	
+	public VillageSansChefException(Throwable cause) {
+		super(cause);
+	}
+	
+	public VillageSansChefException(String message, Throwable cause) {
+		super(message,cause);
+	}
+}
